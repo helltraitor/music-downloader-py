8fcaabbacdec9e78e4aaf4516eaf322a97ee85d3
Helltraitor <helltraitor@hotmail.com>
2022-07-29 23:06:59 +0300

    Added init file to domains with auto import
    Any package must implement Domain abstract class. That allows to register them

---
+++ downloader/domains/__init__.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2022 Helltraitor <helltraitor@hotmail.com>
+#
+# This file is under MIT License (see full license text in music-downloader-py/LICENSE file)
+"""This package contains `Domain` abstract class implementation.
+
+These packages must not be used directly. Any subclass of `Domain` will be
+registered automatically. The registered domains can be found in `domain.py`
+file in `ALL` global variable.
+"""
+import importlib
+import logging
+import pathlib
+
+
+Logger = logging.getLogger(__file__)
+
+
+for entity in pathlib.Path(__file__).parent.iterdir():
+    if entity.name == "__init__.py":
+        continue
+
+    # All domains must implement `Domain` class that will register any subclass
+    #   automatically. So these modules (packages) just need to be imported.
+    importlib.import_module(entity.name)
+    Logger.info("Package %s was successfully imported", entity.name)
